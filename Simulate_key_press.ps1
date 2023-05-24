8babcdcb3572f5808c351d8d0fe491272cddadb2
Alfred Iskhakov <alfred4u@gmail.com>
2023-05-24 23:52:11 +0300

    first commit

---
+++ Simulate_key_press.ps1
@@ -0,0 +1,15 @@
+﻿
+
+# choose your key: https://msdn.microsoft.com/en-us/library/8c6yea83(v=vs.84).aspx
+
+$button = '{F15}' # {NUMLOCK} {CAPSLOCK} {SCROLLLOCK}
+$color=@('gray','red','white', 'green','yellow','blue','magenta','cyan')
+$wsh = New-Object -ComObject WScript.Shell
+
+while (1) {
+  Clear-Host
+  $wsh.SendKeys($button)
+  Write-Host “`n`n`n"
+  Write-Host “Keep alive my PC …” -ForegroundColor (Get-random $color) -BackgroundColor (Get-random $color)
+  Start-Sleep -seconds 5
+}
