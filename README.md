bf57556ff51a7d637193ee5035ec5b4089d860ac
Alfred Iskhakov <alfred4u@gmail.com>
2023-05-25 00:01:36 +0300

    added var $time and readme file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simulate key press
+Script for prevent sleep your PC
+
+# SYNOPSIS
+Script for prevent sleep your PC  
+
+# DESCRIPTION
+You may choose:
+    - any button to be pressed from https://msdn.microsoft.com/en-us/library/8c6yea83(v=vs.84).aspx
+    - time in seconds,
+    - color to be sure that script is on going.
+
+# NOTES
+Enjoy
+
+# EXAMPLE
+```powershell
+.\Simulate_key_press.ps1
+```
--- Simulate_key_press.ps1
+++ Simulate_key_press.ps1
@@ -1,8 +1,9 @@
 ﻿
-
 # choose your key: https://msdn.microsoft.com/en-us/library/8c6yea83(v=vs.84).aspx
 
 $button = '{F15}' # {NUMLOCK} {CAPSLOCK} {SCROLLLOCK}
+$time = 5
+
 $color=@('gray','red','white', 'green','yellow','blue','magenta','cyan')
 $wsh = New-Object -ComObject WScript.Shell
 
@@ -11,5 +12,5 @@ while (1) {
   $wsh.SendKeys($button)
   Write-Host “`n`n`n"
   Write-Host “Keep alive my PC …” -ForegroundColor (Get-random $color) -BackgroundColor (Get-random $color)
-  Start-Sleep -seconds 5
+  Start-Sleep -seconds $time
 }
